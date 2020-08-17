1b7a0f3beccf4a299391aa2bacefc98b0e444eec
Kashif_Ghafoor <58185339+kashif-ghafoor@users.noreply.github.com>
2020-08-16 19:16:43 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("\t\tGuess the movie ");
+public static void main(String[] args) throws Exception {
         RandomMovieName movieName = new RandomMovieName();//getting random name of movie from list of movies
         String randomName=movieName.getRandomMovieName();
+        System.out.println("list of movies is:");
+        for(String movie:movieName.getListOfMovies()) {
+            System.out.println(movie);
+        }
+        System.out.println("////////////////////////////////////////");
+        System.out.println("Game is started");
+        System.out.println("\t\tGuess the movie ");
+
         String hyphenName=randomName;
         hyphenName=hyphenName.replaceAll("[a-zA-Z]","_");//replacing the  character of title with '-'
         GuessingName guessedName=new GuessingName(randomName,hyphenName);//passing random movie name
